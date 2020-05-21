f275d81c63388508bdc01576e30156e6f2804cf9
João Filipe Gonçalves <joaofilipegoncalves2000@gmail.com>
2020-05-21 15:32:55 +0100

    Movimento do player (WIP)

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float maxSpeed = 65.0f;
+
+    Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+
+        Vector2 currentVelocity = rb.velocity;
+
+        currentVelocity = new Vector2(maxSpeed * hAxis, currentVelocity.y);
+
+        rb.velocity = currentVelocity;
+    }
+}
