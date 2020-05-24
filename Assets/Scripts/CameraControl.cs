2c3ac734f64297bdbbec4c95f6c5345d89a883e2
João Filipe Gonçalves <joaofilipegoncalves2000@gmail.com>
2020-05-24 19:16:22 +0100

    Projeto recuperado no master

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField] Transform followTarget;
+    [SerializeField] Vector3 offset;
+    [SerializeField] float feedbackLoopFactor = 0.1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 targetPosition = followTarget.position + offset;
+        targetPosition.z = transform.position.z;
+
+        Vector3 delta = targetPosition - transform.position;
+
+        transform.position = transform.position + delta * feedbackLoopFactor;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float maxSpeed = 50.0f;
+
+    Rigidbody2D rb;
+
+    Animator anim; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+
+        Vector2 currentVelocity = rb.velocity;
+
+        currentVelocity = new Vector2(maxSpeed * hAxis, currentVelocity.y);
+
+        rb.velocity = currentVelocity;
+
+        anim.SetFloat("AbsVelx", Mathf.Abs(currentVelocity.x));
+
+        //Changes walking/runing directions
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.rotation = Quaternion.identity;
+        }
+
+        //Run
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            maxSpeed = 80.0f;
+        }
+        else
+        {
+            maxSpeed = 50.0f;
+        }
+    }
+}
