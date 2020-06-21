94cc13edea3b24c271fa399750a7c44dc1f94c0d
JoaoFonseca197 <joao.hc.fonseca197@gmail.com>
2020-06-21 11:13:54 +0100

    new scene

---
+++ Assets/Scripts/EnemyPlayerDetection.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPlayerDetection : MonoBehaviour
+{
+    public LayerMask        visionLayer;
+
+
+    public GameObject      Player;
+
+    Rigidbody2D rigidBody;
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        
+        Vector2 playerPosition = Player.transform.position;
+        Vector2 currentDirection = ( playerPosition - rigidBody.position );
+        float distance = currentDirection.magnitude;
+
+        //if hits something between the player and the enemy comes true if not comes false
+        var hit = Physics2D.Raycast(rigidBody.position, currentDirection.normalized,distance, visionLayer);
+
+        //if hits something between the player and the enemy prints can´t see else checks if the player is in the cone of vision os the enemy
+        if(hit == true )
+        {
+                Debug.Log("this CAN´t SEE");
+               
+        }
+        else
+        {
+            
+            Debug.Log(rigidBody.velocity);
+            float viewAngle = Vector2.Dot(rigidBody.velocity.normalized, currentDirection.normalized);
+            Debug.Log(viewAngle);
+            if (viewAngle > 0 || viewAngle == 0)
+            {
+                Debug.Log("u can see");
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/EnemyWalk.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWalk : MonoBehaviour
+{
+
+    [SerializeField] float      moveSpeed = 65.0f;
+    [SerializeField] Transform  wallDetector = null;
+    [SerializeField] float      detectionRadius = 3.0f;
+    [SerializeField] LayerMask  wallLayers;
+
+    
+    Rigidbody2D rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.velocity = new Vector2(moveSpeed, 0.0f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 currentVelocity = rigidBody.velocity;
+
+        currentVelocity.x = moveSpeed;
+
+        rigidBody.velocity = currentVelocity;
+        if(wallDetector)
+        {
+            Collider2D wallCollision = Physics2D.OverlapCircle(wallDetector.position, detectionRadius, wallLayers);
+
+            bool onWall = wallCollision != null;
+            
+            if(onWall)
+            {
+                TurnBack();
+            }
+
+            currentVelocity.x = transform.right.x * moveSpeed;
+
+            rigidBody.velocity = currentVelocity;
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (wallDetector != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(wallDetector.position, detectionRadius);
+        }
+    }
+
+
+    void TurnBack()
+    {
+        transform.rotation = transform.rotation * Quaternion.Euler(0, 180, 0);
+    }
+}
