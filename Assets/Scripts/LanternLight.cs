5120e9fed263e80ced1fb5972b2dc2c88ac728b5
João Filipe Gonçalves <joaofilipegoncalves2000@gmail.com>
2020-06-22 14:57:05 +0100

    Lantern light working

---
+++ Assets/Scripts/LanternLight.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LanternLight : MonoBehaviour
+{
+    public GameObject theObject;
+    bool lighton = false;
+    Light2D lantern;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        theObject = GameObject.Find("LanternLight");
+        lantern = theObject.GetComponent<Light2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void Use()
+    {
+        if (lighton == false)
+        {
+            lighton = true;
+            lantern.intensity = 1;
+        }
+        else
+        {
+            lighton = false;
+            lantern.intensity = 0;
+        }
+    }
+}
