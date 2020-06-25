f01d157eec20c2e2324bf3df76030587593ab974
João Filipe Gonçalves <joaofilipegoncalves2000@gmail.com>
2020-06-25 01:43:05 +0100

    Bateries system for lantern and lvl 1 struct

--- Assets/Scripts/LanternLight.cs
+++ Assets/Scripts/LanternLight.cs
@@ -11,6 +11,9 @@ public class LanternLight : MonoBehaviour
     public AudioClip onSound;
     public AudioClip offSound;
     public GameObject theObject;
+    public float lifeTime = 300.0f;
+    public float currentTime;
+    GameObject pilha;
     bool lighton = false;
     Light2D lantern;
     
@@ -20,27 +23,42 @@ public class LanternLight : MonoBehaviour
     {
         theObject = GameObject.Find("LanternLight");
         lantern = theObject.GetComponent<Light2D>();
+
+        currentTime = lifeTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        pilha = GameObject.FindWithTag("Pilha");
+        if (lighton == true)
+        {
+            currentTime = lifeTime -= 1 * Time.deltaTime;
+            if (currentTime <= 0)
+            {
+                currentTime = 0;
+                lantern.enabled = false;
+                Destroy(pilha);
+            }
+        }
     }
 
     public void Use()
     {
-        if (lighton == false)
+        if (pilha == true)
         {
-            lighton = true;
-            lantern.enabled = true;
-            SoundMng.instance.PlaySound(onSound);
-        }
-        else
-        {
-            lighton = false;
-            lantern.enabled = false;
-            SoundMng.instance.PlaySound(offSound);
+            if (lighton == false)
+            {
+                lighton = true;
+                lantern.enabled = true;
+                SoundMng.instance.PlaySound(onSound);
+            }
+            else
+            {
+                lighton = false;
+                lantern.enabled = false;
+                SoundMng.instance.PlaySound(offSound);
+            }
         }
     }
 }
