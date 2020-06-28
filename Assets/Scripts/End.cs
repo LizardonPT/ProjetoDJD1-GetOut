8234a1772b9c2cfc4faba7dd71c0d7387d35d45b
LizardonPT <joaofilipegoncalves2000@gmail.com>
2020-06-29 00:18:03 +0100

    Win condition

---
+++ Assets/Scripts/End.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End : MonoBehaviour
+{
+    GameObject player;
+    GameObject inventory;
+
+    Vector2 playerPos;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerPos = player.transform.position;
+        inventory = GameObject.Find("Canvas");
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.name == "Player")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                SceneManager.LoadScene("Win End");
+                Destroy(inventory);
+            }
+        }
+    }
+}
